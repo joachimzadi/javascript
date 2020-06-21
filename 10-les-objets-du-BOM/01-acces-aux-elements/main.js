1de1640b11bc749e13a87766bea24dcae49fa1b9
Joachim Zadi <joachim.zadi@j4ltechnologies.com>
2020-06-21 20:07:30 +0200

    Commit 10-les-objets-du-BOM

---
+++ 10-les-objets-du-BOM/01-acces-aux-elements/main.js
@@ -0,0 +1,23 @@
+// Crée par Joachim Zadi le 21/06/2020 à 16:24
+// ===========================================
+
+// Recherche d'un element à partir du nom de tag
+// *********************************************
+
+// TODO
+
+// Recherche d'un element à partir de son Id
+// *****************************************
+
+// TODO
+
+// Recherche d'un element à partir du nom de sa classe CSS
+// *******************************************************
+
+// TODO
+
+// Recherche d'un element à partir du nom de son delecteur CSS
+// ***********************************************************
+
+// TODO
+
---
+++ 10-les-objets-du-BOM/02-modifier-les-elts/main.js
@@ -0,0 +1,32 @@
+// Crée par Joachim Zadi le 21/06/2020 à 16:24
+// ===========================================
+
+// Recuperer le contenu du tag h1
+// ******************************
+
+//TODO
+
+// Modifier le contenu du tag h1
+// *****************************
+
+//TODO
+
+// Modifier le contenu HTML du tag h1
+// **********************************
+
+//TODO
+
+// Exercices
+// *********
+
+//TODO
+
+// Modifier l'attribut d'un element
+// ********************************
+
+//TODO
+
+// Modifier le style d'un element
+// ******************************
+
+//TODO
