1bbc9a607d3616f862fba43de82558f0c9f0f003
J4LPROJECTS <gitprojects@j4ltechnologies.com>
2020-06-24 09:03:25 +0200

    Organisation des projets fini

--- 10-les-objets-du-BOM/07-les-formulaires/main.js
+++ 10-les-objets-du-BOM/07-les-formulaires/main.js
@@ -1,7 +1,3 @@
 // Crée par Joachim Zadi le 24/06/2020 à 05:15
 // ===========================================
-let saisie = document.getElementById('saisie');
-let erreur = document.getElementById('erreur');
-let formulaire = document.getElementById('formulaire');
 
-erreur.style.display = 'none';
